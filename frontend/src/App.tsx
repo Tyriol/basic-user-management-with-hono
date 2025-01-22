69e7c75d7243f6f3838abe8a40bdb879e9beec5e
Ryan <ryanshaunsmith@gmail.com>
2025-01-22 10:39:10 +0000

    consistent formatting using prettier

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,91 +5,95 @@ import Form from './components/Form/Form.tsx'
 import UserCard from './components/UserCard/UserCard.tsx'
 
 function App() {
-    const [users, setUsers] = useState<Users>([]);
-    const [error, setError] = useState<string | null>(null);
-    const [editingUser, setEditingUser] = useState<User | null>(null)
+  const [users, setUsers] = useState<Users>([])
+  const [error, setError] = useState<string | null>(null)
+  const [editingUser, setEditingUser] = useState<User | null>(null)
 
-    useEffect(() => {
-        const fetchUsers = async () => {
-            try {
-                const response = await fetch('http://localhost:3000/api/users')
-                let userData = await response.json()
-                setUsers(userData.users)
-            } catch (err: any) {
-                setError(err.message)
-                console.error(error)
-                setUsers([])
-            }
-        }
-        fetchUsers()
-    }, [])
-
-    const addUser = async (newUser: User) => {
-      setUsers([...users, newUser]);
-      await fetch(
-        "http://localhost:3000/api/users",
-        {
-          method: "POST",
-          headers: {
-            Accept: "application/json",
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify(newUser),
-        }
-      );
+  useEffect(() => {
+    const fetchUsers = async () => {
+      try {
+        const response = await fetch('http://localhost:3000/api/users')
+        let userData = await response.json()
+        setUsers(userData.users)
+      } catch (err: any) {
+        setError(err.message)
+        console.error(error)
+        setUsers([])
+      }
     }
+    fetchUsers()
+  }, [])
 
-    const updateUser = async (updatedUser: User) => {
-      const updatedUsers = users.map(user => 
-        user.id === updatedUser.id ? updatedUser : user
-      );
-      setUsers(updatedUsers);
-      await fetch(
-        `http://localhost:3000/api/users/${updatedUser.id}`,
-        {
-          method: "PUT",
-          headers: {
-            Accept: "application/json",
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify(updatedUser),
-        }
-      );
-    }
+  const addUser = async (newUser: User) => {
+    setUsers([...users, newUser])
+    await fetch('http://localhost:3000/api/users', {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(newUser),
+    })
+  }
 
-    const deleteUser = async (userToDelete: User) => {
-      const response = await fetch(
-        `http://localhost:3000/api/users/${userToDelete.id}`,
-        {
-          method: "DELETE",
-          headers: {
-            Accept: "application/json",
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify(userToDelete),
-        }
-      );
-      const usersAfterDelete = await response.json();      
-      setUsers(usersAfterDelete.usersAfterDelete);
-    }
-
-    return (
-        <>
-            <header>
-                <h1>User Management</h1>
-            </header>
-            <main>
-                <Form addUser={addUser} updateUser={updateUser} editingUser={editingUser} setEditingUser={setEditingUser} />
-                <ul>
-                    {users.map((user: User) => {
-                        return (
-                            <UserCard user={user} deleteUser={deleteUser} setEditingUser={setEditingUser}/>
-                        )
-                    })}
-                </ul>
-            </main>
-        </>
+  const updateUser = async (updatedUser: User) => {
+    const updatedUsers = users.map((user) =>
+      user.id === updatedUser.id ? updatedUser : user
     )
+    setUsers(updatedUsers)
+    await fetch(`http://localhost:3000/api/users/${updatedUser.id}`, {
+      method: 'PUT',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(updatedUser),
+    })
+  }
+
+  const deleteUser = async (userToDelete: User) => {
+    const response = await fetch(
+      `http://localhost:3000/api/users/${userToDelete.id}`,
+      {
+        method: 'DELETE',
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(userToDelete),
+      }
+    )
+    const usersAfterDelete = await response.json()
+    setUsers(usersAfterDelete.usersAfterDelete)
+  }
+
+  return (
+    <>
+      <header>
+        <h1>User Management</h1>
+      </header>
+      <main>
+        <Form
+          addUser={addUser}
+          updateUser={updateUser}
+          editingUser={editingUser}
+          setEditingUser={setEditingUser}
+        />
+        <ul>
+          {users.map((user: User) => {
+            return (
+              <UserCard
+                key={user.id}
+                user={user}
+                deleteUser={deleteUser}
+                setEditingUser={setEditingUser}
+              />
+            )
+          })}
+        </ul>
+      </main>
+    </>
+  )
 }
 
 export default App
--- frontend/src/components/UserCard/UserCard.tsx
+++ frontend/src/components/UserCard/UserCard.tsx
@@ -1,28 +1,24 @@
-import type { User } from "../../../../backend/types/types";
+import type { User } from '../../../../backend/types/types'
 
 interface UserCardProps {
-    user: User;
-    deleteUser: (user: User) => void;
-    setEditingUser: (user: User) => void;
+  user: User
+  deleteUser: (user: User) => void
+  setEditingUser: (user: User) => void
 }
 
-function UserCard ({user, deleteUser, setEditingUser}: UserCardProps) {
-    return(
-        <li className="user" key={user.id}>
-            <div className="user-name">
-                Name: {user.name}
-            </div>
-            <div className="user-id">ID: {user.id}</div>
-            <div className="email-address">
-                Email Address: {user.email}
-            </div>
-            <div className="role">Role: {user.role}</div>
-            <div className="actions">
-                <button onClick={() => setEditingUser(user)}>Update</button>
-                <button onClick={() => deleteUser(user)}>Delete</button>
-            </div>
-        </li>
-    )
+function UserCard({ user, deleteUser, setEditingUser }: UserCardProps) {
+  return (
+    <li className="user">
+      <div className="user-name">Name: {user.name}</div>
+      <div className="user-id">ID: {user.id}</div>
+      <div className="email-address">Email Address: {user.email}</div>
+      <div className="role">Role: {user.role}</div>
+      <div className="actions">
+        <button onClick={() => setEditingUser(user)}>Update</button>
+        <button onClick={() => deleteUser(user)}>Delete</button>
+      </div>
+    </li>
+  )
 }
 
-export default UserCard;
+export default UserCard
